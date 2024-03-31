8f581375cb09b0ded1e4d1c350f05c8b7c0dcc62
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2024-03-31 20:31:28 +0900

    リファクタリング
    (1) 共通のカスタマイズを親リポジトリから移動

---
+++ CommonCustomize.rb
@@ -0,0 +1,30 @@
+##  -*-  coding: utf-8; mode: ruby -*-  ##
+##  vi: set ft=ruby
+
+Vagrant.configure("2") do |config|
+  config.vm.box_download_options = {"ssl-revoke-best-effort" => true}
+
+  config.vm.provider "virtualbox" do |v|
+    v.customize [
+      "modifyvm",           :id,
+      "--cableconnected1",  "on",
+      "--hwvirtex",         "on",
+      "--nestedpaging",     "on",
+      "--largepages",       "on",
+      "--ioapic",           "on",
+      "--pae",              "on",
+      "--paravirtprovider", "kvm",
+    ]
+
+    #
+    # 時刻をホスト側と同期する
+    #
+    v.customize [
+      'setextradata',     :id,
+      'VBoxInternal/Devices/VMMDev/0/Config/GetHostTimeDisabled',
+      0
+    ]
+  end
+
+  config.vm.boot_timeout = 900
+end
