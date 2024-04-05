d2d92058007aefdfd71e129113b23c9f487bf07a
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2024-04-05 23:03:46 +0900

    プロビジョニングが実行済みであることを記録

--- provision.ramdisk.sh
+++ provision.ramdisk.sh
@@ -1,5 +1,7 @@
 #!/bin/bash  -xue
 
+test  -f "/root/.provision/ramdisk"  &&  exit 0
+
 # RamDisk
 sudo  mkdir  -p    /ramdisk
 sudo  chmod  1777  /ramdisk
@@ -9,3 +11,6 @@ echo  -e  "tmpfs\t/ramdisk\ttmpfs\trw,size=2048m,x-gvfs-show\t0\t0"  \
 
 sudo  mount  -a
 sudo  chmod  1777  /ramdisk
+
+mkdir -p "/root/.provision"
+date  >  "/root/.provision/ramdisk"
