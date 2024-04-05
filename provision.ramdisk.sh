d6f66d67677cba34e830c2bcf565f56a49cd547d
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2024-04-05 22:55:57 +0900

    不具合修正

--- provision.ramdisk.sh
+++ provision.ramdisk.sh
@@ -1,7 +1,7 @@
 #!/bin/bash  -xue
 
 # RamDisk
-sudo  mkdir        /ramdisk
+sudo  mkdir  -p    /ramdisk
 sudo  chmod  1777  /ramdisk
 
 echo  -e  "tmpfs\t/ramdisk\ttmpfs\trw,size=2048m,x-gvfs-show\t0\t0"  \
