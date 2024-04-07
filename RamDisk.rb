1d0fc89f1a3b80fb9bb5df4f20566eeb05b50616
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2024-04-07 10:55:12 +0900

    不具合修正

--- RamDisk.rb
+++ RamDisk.rb
@@ -2,7 +2,7 @@
 ##  vi: set ft=ruby
 
 Vagrant.configure("2") do |config|
-  config.vm.provision("newhdd", type: "shell",
-                      path: "#{__dir__}/provision/newhdd.sh",
+  config.vm.provision("ramdisk", type: "shell",
+                      path: "#{__dir__}/provision/ramdisk.sh",
                       privileged: true)
 end
