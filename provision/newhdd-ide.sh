a4b93892d39dc8c01d26cf7244541614ef2ec3a7
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2024-04-19 22:48:01 +0900

    対象のハードディスクを修正

--- provision/newhdd-ide.sh
+++ provision/newhdd-ide.sh
@@ -1,6 +1,6 @@
 #!/bin/bash  -xue
 
-test  -f "/root/.provision/newhdd"  &&  exit 0
+test  -f "/root/.provision/newhdd-ide"  &&  exit 0
 
 # New HDD (/dev/sdc)
 cat <<  __EOF__  |  tee  /dev/shm/zero.md5
@@ -11,25 +11,27 @@ cat <<  __EOF__  |  tee  /dev/shm/check_mbr.md5
 0a4dbaf15747a2f282529ebec196a2d4 *-
 __EOF__
 
+trg_hdd='/dev/sdb'
+
 # MBR/GPT ヘッダ 3 セクタ 768 バイトが全部ゼロなら未初期化と判定
-sudo  dd if=/dev/sdc bs=512 count=3 | md5sum -b
-if sudo dd if=/dev/sdc bs=512 count=3 | md5sum -c /dev/shm/zero.md5 ; then
-    sudo  parted --script --align optimal /dev/sdc -- mklabel gpt
-    sudo  parted --script --align optimal /dev/sdc -- mkpart primary ext3 1 -1
-    sudo  mkfs.ext3     /dev/sdc1
+sudo  dd if=${trg_hdd} bs=512 count=3 | md5sum -b
+if sudo dd if=${trg_hdd} bs=512 count=3 | md5sum -c /dev/shm/zero.md5 ; then
+    sudo  parted --script --align optimal ${trg_hdd} -- mklabel gpt
+    sudo  parted --script --align optimal ${trg_hdd} -- mkpart primary ext3 1 -1
+    sudo  mkfs.ext3     "${trg_hdd}1"
 else
     echo "Disk Already Formatted."  1>&2
     sleep 5
 fi
 
 # GPT ヘッダは毎回変わるようなので、MBR ヘッダだけ確認する
-sudo  dd if=/dev/sdc bs=512 count=1 | md5sum -b
-sudo  dd if=/dev/sdc bs=512 count=1 | md5sum -c /dev/shm/check_mbr.md5
+sudo  dd if=${trg_hdd} bs=512 count=1 | md5sum -b
+sudo  dd if=${trg_hdd} bs=512 count=1 | md5sum -c /dev/shm/check_mbr.md5
 
 sudo  mkdir  -p    /ext-hdd/data
 sudo  chmod  1777  /ext-hdd/data
 
-echo  -e  "/dev/sdc1\t/ext-hdd/data\text3\tdefaults\t0\t0"  \
+echo  -e  "${trg_hdd}1\t/ext-hdd/data\text3\tdefaults\t0\t0"  \
     |  sudo  tee -a  /etc/fstab
 
 sudo  mount  -a
@@ -39,4 +41,4 @@ ls -al /ext-hdd/
 sleep 5
 
 mkdir -p "/root/.provision"
-date  >  "/root/.provision/newhdd"
+date  >  "/root/.provision/newhdd-ide"
