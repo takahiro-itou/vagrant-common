bd025836495eebe7871bc475206204979dfe1d73
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2024-04-21 12:20:58 +0900

    リファクタリング

--- provision/hguser.sh
+++ provision/hguser.sh
@@ -11,25 +11,25 @@ user_pass='\$6\$F1IrDR2U\$'
 user_pass+='gdsuepzT7noxUWaA5cpRjfAdhE5cS6qh0WhNBYm83ey'
 user_pass+='PiZ0XbfrzuG6dGH3aezIQWn.vRf.aJsz1qD3RMi7yj0'
 
-newUser=hg
-new_user_group=hg
+new_user='hg'
+new_user_group='hg'
 
 sudo  groupadd  "${new_user_group}"
 userIndex=0
 
-echo  "Initialize User: ${HOSTNAME}  ${newUser}"
+echo  "Initialize User: ${HOSTNAME}  ${new_user}"
 
 # ユーザーを追加する。
-new_user_add_opts="-g ${new_user_group}  -d /home/${newUser}  -s /bin/bash"
-sudo  useradd  "${new_user_add_opts}"  -m "${newUser}"
-eval  new_user_home="~${newUser}"
+new_user_add_opts="-g ${new_user_group}  -d /home/${new_user}  -s /bin/bash"
+sudo  useradd  "${new_user_add_opts}"  -m "${new_user}"
+eval  new_user_home="~${new_user}"
 
 # ユーザーのパスワードを設定する。
 new_passwd="${user_pass}"
-sedPat="^${newUser}:!:.*\$"
-sedRep="${newUser}:${new_passwd}:18294:0:99999:7:::"
-sedCmd="s|${sedPat}|${sedRep}|"
-command="sudo  sed -i.bak  -e '${sedCmd}'  /etc/shadow"
+sed_pat="^${new_user}:!:.*\$"
+sed_rep="${new_user}:${new_passwd}:18294:0:99999:7:::"
+sed_cmd="s|${sed_pat}|${sed_rep}|"
+command="sudo  sed -i.bak  -e '${sed_cmd}'  /etc/shadow"
 echo  "Execute: ${command}"
 eval  ${command}
 
@@ -49,7 +49,7 @@ if [[ -f "${new_user_auth}" ]] ; then
 fi
 if [[ -d "${new_user_ssh}" ]] ; then
     sudo  chmod  0700  "${new_user_ssh}"
-    sudo  chown  -R  "${newUser}:${new_user_group}"  "${new_user_ssh}"
+    sudo  chown  -R  "${new_user}:${new_user_group}"  "${new_user_ssh}"
 fi
 
 mkdir -p "${HOME}/.provision"
