153d6a51839b41cbff6e391f890ec7e1832e673a
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2025-11-08 18:23:38 +0900

    リファクタリング

--- SetupUbuntu.rb
+++ SetupUbuntu.rb
@@ -1,8 +1,12 @@
 ##  -*-  coding: utf-8; mode: ruby -*-  ##
 ##  vi: set ft=ruby
 
+def provision_setup_ubuntu(vm)
+  vm.provision("setup-time", type: "shell",
+                path: "#{__dir__}/provision/setup-time.sh",
+                privileged: true)
+end
+
 Vagrant.configure("2") do |config|
-  config.vm.provision("setup-time", type: "shell",
-                      path: "#{__dir__}/provision/setup-time.sh",
-                      privileged: true)
+  provision_setup_ubuntu(config.vm)
 end
