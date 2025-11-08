2a2a00c5b8113245b90215c0b40fcbe48ccf1c84
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2025-11-08 18:22:49 +0900

    リファクタリング

--- UploadFiles.rb
+++ UploadFiles.rb
@@ -1,11 +1,17 @@
 ##  -*-  coding: utf-8; mode: ruby -*-  ##
 ##  vi: set ft=ruby
 
+def provision_upload_files(vm)
+
+  vm.provision("userfiles1", type: "file",
+                source: "data.tar.xz",
+                destination: "/tmp/")
+  vm.provision("userfiles2", type: "shell",
+                path: "common/provision/data.sh",
+                privileged: false)
+
+end
+
 Vagrant.configure("2") do |config|
-  config.vm.provision("userfiles1", type: "file",
-                      source: "data.tar.xz",
-                      destination: "/tmp/")
-  config.vm.provision("userfiles2", type: "shell",
-                      path: "common/provision/data.sh",
-                      privileged: false)
+  provision_upload_files(config.vm)
 end
