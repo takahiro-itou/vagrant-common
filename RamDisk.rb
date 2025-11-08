f702b0c32e846a8b0ca0214dd21d0c23c56e7b27
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2025-11-08 18:24:28 +0900

    リファクタリング

--- RamDisk.rb
+++ RamDisk.rb
@@ -1,8 +1,12 @@
 ##  -*-  coding: utf-8; mode: ruby -*-  ##
 ##  vi: set ft=ruby
 
+def provision_ram_disk(vm)
+  vm.provision("ramdisk", type: "shell",
+                path: "#{__dir__}/provision/ramdisk.sh",
+                privileged: true)
+end
+
 Vagrant.configure("2") do |config|
-  config.vm.provision("ramdisk", type: "shell",
-                      path: "#{__dir__}/provision/ramdisk.sh",
-                      privileged: true)
+  provision_ram_disk(config.vm)
 end
