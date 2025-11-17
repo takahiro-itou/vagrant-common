1e5384bdee00b137130cd5e41656f234dc251063
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2025-11-17 12:42:08 +0900

    例外処理を追加

--- storage/add-scsi-controller.sh
+++ storage/add-scsi-controller.sh
@@ -6,6 +6,23 @@ machine=$1
 name=${2:-'SCSI'}
 maxport=${3:-'16'}
 
+
+function  add_scsi_ctl::enumerate_storage_controllers () {
+
+VBoxManage  \
+    showvminfo "${machine}"  --machinereadable  | \
+    grep  storagecontrollername
+
+}
+
+function  add_scsi_ctl::main () {
+
+if [[ add_scsi_ctl::enumerate_storage_controllers | grep "${name}" ]] ; then
+    # 既にコントローラが存在するので無視
+    echo  "Controller ${name} already exists. SKIP."  1>&2
+    exit  0
+fi
+
 VBoxManage  \
     storagectl      "${machine}"    \
     --name          "${name}"       \
@@ -14,3 +31,8 @@ VBoxManage  \
     --portcount     "${maxport}"    \
     --bootable      on              \
 ;
+
+}
+
+
+add_scsi_ctl::main
